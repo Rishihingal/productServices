26c6dd186cf888174e08fa169b3024a25b744ae5
rishihingal <rishihingal@gmail.com>
2023-09-06 18:19:51 +0530

    Setup the Springboot project and created the models and controller for Product services

---
+++ src/main/java/dev/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package dev.productservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/products")
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    @GetMapping("{id}")
+    public String getProductbyId(@PathVariable("id") long id) {
+        return "Here is product id: " + id;
+    }
+    @GetMapping()
+    public void getAllProducts() {
+
+    }
+
+    @PostMapping()
+    public void createProduct(){
+
+    }
+    @DeleteMapping("{id}")
+    public void deleteProduct(){
+
+    }
+
+    @PutMapping("{id}")
+    public void updateProduct(){
+
+    }
+}
---
+++ src/main/java/dev/productservice/models/BaseModel.java
@@ -0,0 +1,5 @@
+package dev.productservice.models;
+
+public class BaseModel {
+    private long id;
+}
